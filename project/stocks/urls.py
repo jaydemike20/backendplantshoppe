e9403659a36348130f623063d923ae1bdf93a77c
jaydemike <jaydemike21@gmail.com>
2023-09-23 12:21:03 +0800

    Some changes

--- project/stocks/urls.py
+++ project/stocks/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 from stocks.views import PlantsListCreateAPIView, PlantsRetrieveUpdateDestroyAPIView, OrderListCreateAPIView, OrderRetrieveUpdateDestroyAPIView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('plants/', PlantsListCreateAPIView.as_view(), name="plants-list" ),
